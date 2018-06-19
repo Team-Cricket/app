2d409976818dac485c32cd1f18711687029dea40
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-19 11:52:41 -0700

    Adding inputs, etc.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
-    <h1>Login Form Component</h1>
+
+    <img src="https://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/v1/983693/axmqlpjyo3zmeszdr9qt.png">
     <button @click="type = isSignUp ? 'signIn': 'signUp'">
       {{ isSignUp
           ? "Have an account? Click here to sign in"
           : "Click here to sign up"
       }}
     </button>
+    <form @submit.prevent="handleSubmit">
+      <input
+      :type="show ? 'text' : 'password'"
+      v-model="credentials.email">
+
+      <button
+      @click="show = !show"
+      type="button"
+      >
+        {{ show? 'Hide' : 'Show' }}
+      </button>
+
+      <button type="submit"
+        {{ label }}
+      >
+      </button>
+    </form>
+
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: ''
+      }
+    };
+  }
   
-
 };
 </script>
 
