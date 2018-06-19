7d0205bf95117a9e67c1af2470dc6424f107b799
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-19 14:37:33 -0700

    Added signUp, signIn functions

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const URL = 'http://localhost:3000/api';
 const EVENTS_URL = `${URL}/events`;
+const AUTH_URL = `${URL}/auth`;
 
 function responseHandler(response) {
   if(response.ok) return response.json();
@@ -11,6 +12,24 @@ function responseHandler(response) {
 export function getEvents(userID) {
   return fetch(`${EVENTS_URL}/${userID}`, {
     headers: { 'Content-Type': 'application/json' }
+  })
+    .then(responseHandler);
+}
+
+export function signUp(credentials) {
+  return fetch(`${AUTH_URL}/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(credentials)
+  })
+    .then(responseHandler);
+}
+
+export function signIn(credentials) {
+  return fetch(`${AUTH_URL}/signin`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(credentials)
   })
     .then(responseHandler);
 }
