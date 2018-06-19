d39c45be6645ab12f5bec4261a3ef73407535b16
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-19 10:59:19 -0700

    Adding login form component details

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Login Form Component</h1>
+  <div>
+    <h1>Login Form Component</h1>
+    <button @click="type = isSignUp ? 'signIn': 'signUp'">
+      {{ isSignUp
+          ? "Have an account? Click here to sign in"
+          : "Click here to sign up"
+      }}
+    </button>
+  </div>
 </template>
 
 <script>
