36a8f918f08491e2b715a168368f9c57485ca553
carriecarter <carrie@carrie-carter.com>
2018-06-19 11:06:34 -0700

    routerjs created

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>I am the contacts list</h2>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,3 +1,27 @@
 import VueRouter from 'vue-router';
+import LoginForm from './components/LoginForm.vue';
+import NavBar from './components/NavBar.vue';
+import EventForm from './components/EventForm.vue';
+import ContactForm from './components/ContactForm';
+import DashboardView from './components/DashboardView';
+import EventsList from './components/EventsList';
+import ContactsList from './components/ContactsList';
 
-//export default new VueRouter
+
+export default new VueRouter({
+  routes: [
+    { path: '/', component: LoginForm },
+    { path: '/', component: NavBar },
+    { path: '/event', component: EventForm },
+    { path: '/contact', component: ContactForm },
+    { 
+      path: '/dashboard', 
+      component: DashboardView,
+      children: [
+        { path: 'events', component: EventsList },
+        { path: 'contacts', component: ContactsList },
+      ]
+    },
+    { path: '*', redirect: '/' }
+  ]
+});
