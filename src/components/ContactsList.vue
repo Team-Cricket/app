ccb49971fc090c4400b42af98f123fb5d9fdd8b4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-20 16:24:31 -0700

    Creating ContactList, Dashboard, EventList details in rendering

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,5 +1,21 @@
 <template>
-  <h2>I am the contacts list</h2>
+  <div>
+    <div id="contacts">
+      <pre v-if="error">{{ error }}</pre>
+      <ul v-if="contacts !== null">
+        <li
+          v-for="contact in contacts"
+          :key="contact.contactId"
+        >
+          <!-- <router-link :to="`/events/${user.userId}`"> -->
+          <router-link :to="`/contacts`"> 
+            <strong>{{ contact.name }}</strong><br>
+          </router-link>
+          {{ event.description }}<br>&nbsp;
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,27 +1,25 @@
 <template>
-  <div id="events">
-    <h2>Events List</h2>
-    <button v-on:click="openNewEvent">add event</button>
-    <pre v-if="error">{{ error }}</pre>
-    <ul v-if="events !== null">
-      <li
-        v-for="event in events"
-        :key="event.eventId"
-      >
-        <!-- <router-link :to="`/events/${user.userId}`"> -->
-        <router-link :to="`/events`"> 
-          <strong>{{ event.name }}</strong> ({{ event.count }} contacts)<br>
-        </router-link>
-        {{ event.description }}<br>&nbsp;
-      </li>
-    </ul>
+  <div>
+    <div id="events">
+      <pre v-if="error">{{ error }}</pre>
+      <ul v-if="events !== null">
+        <li
+          v-for="event in events"
+          :key="event.eventId"
+        >
+          <!-- <router-link :to="`/events/${user.userId}`"> -->
+          <router-link :to="`/events`"> 
+            <strong>{{ event.name }}</strong> ({{ event.count }} contacts)<br>
+          </router-link>
+          {{ event.description }}<br>&nbsp;
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-.push({name: "Add new company", id=0})
-
 <script>
-import { getEvents } from '../services/api';
+import { getEvents, deleteEvent } from '../services/api';
 
 export default {
   data() {
@@ -43,9 +41,6 @@ export default {
   },
   props: ['user'],
   methods: {
-    openNewEvent() {
-      this.$router.push('/event');
-    },
     deleteEvent(event) {
       const id = event.eventId;
       // remove from server
@@ -63,13 +58,20 @@ export default {
 
 
 <style scoped>
-
+#events {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
 div {
   text-align: left;
 }
 
 ul {
   list-style-type: none;
+}
+li {
+  max-width: 375px;
 }
 
 
