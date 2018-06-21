77508add62715413ca88cca0daedfab73ca4a475
Kevin Grayson <k.grayson.ms@gmail.com>
2018-06-21 14:06:41 -0700

    Updating events working

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -9,8 +9,9 @@
         >
           <!-- <router-link :to="`/events/${user.userId}`"> -->
             <img class="delete-logo" @click="handleDelete(event)" width="15px" src="../assets/delete-icon.png">
-          <router-link :to="`/events`"> 
+          <router-link :to="`/event/${event.eventId}`"> 
             <h3>{{ event.name }}</h3> 
+            <h3>{{ event.eventDate.substring(0, 10) }}</h3> 
             <h4>{{ event.count }} contacts</h4>
             <br>
           </router-link>
